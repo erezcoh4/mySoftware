a5a59fed8f64dfac5c55216a20774708efb45f52
Erez Cohen <erezcohen@Erezs-iMac.local>
2016-04-16 20:04:34 +0300

    april 16

---
+++ MySoftwarePackage/mac/Initiation.py
@@ -0,0 +1,17 @@
+import ROOT , sys , os , time
+ROOT.gStyle.SetOptStat(0000)
+
+def dirname( ):
+    dirfmt = "/Users/erezcohen/Desktop/%4d-%02d-%02d"
+    dirname = dirfmt % time.localtime()[0:3]
+    return dirname;
+
+def createnewdir( ):
+    dirfmt = "/Users/erezcohen/Desktop/%4d-%02d-%02d"
+    dirname = dirfmt % time.localtime()[0:3]
+    try:
+        os.makedirs(dirname)
+    except OSError, e:
+        if e.errno != 17:
+            raise # This was not a "directory exist" error..
+        return

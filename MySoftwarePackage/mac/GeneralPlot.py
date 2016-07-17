e34a349c5effe083abdf90723dede32dc3d708f3
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-07-17 17:00:01 +0300

    Sun Jul 17 17:00:01 IDT 2016

--- MySoftwarePackage/mac/GeneralPlot.py
+++ MySoftwarePackage/mac/GeneralPlot.py
@@ -7,6 +7,32 @@ import Initiation as init
 init.createnewdir()
 ROOT.gStyle.SetOptStat(0000)
 
+
+PmissTit        = "|#vec{p}_{miss}| [GeV/c]"
+PcmTit          = "|#vec{p} (c.m.)| [GeV/c]"
+Q2Tit           = "Q^{2} (GeV/c) ^{2}"
+XbTit           = "Bjorken x"
+qp1p2Tit        = "#sqrt{|q - p_{1} - p_{2}|^{2} } [GeV/c ^{2}]"
+WeTit           = "W_{e} ~ #sqrt{|M^{2}_{p} + 2M_{p}#omega - Q^{2}|} [GeV/c ^{2}]"
+We2Tit          = "W^{2}_{e} ~ M^{2}_{p} + 2M_{p}#omega - Q^{2} (GeV/c ^{2}) ^{2}"
+WmissTit        = "#sqrt{|3M_{N} + q - p_{1} - p_{2}|^{2} } [GeV/c ^{2}]"
+WmCmTit         = "#sqrt{ |(3M_{N},#vec{k}_{cm}) + q - p_{1} - p_{2}|^{2} } [GeV/c ^{2}]"
+WmEpsTit        = "#sqrt{ |(3M_{N}-#varepsilon,#vec{k}_{cm}) + q - p_{1} - p_{2}|^{2} } [GeV/c ^{2}]"
+PmqTit          = "|#vec{p}_{miss} #hat{q}|"
+PrecqTit        = "|#vec{p}_{rec} #hat{q}|"
+ThetaPrecTit    = "#theta(#vec{p}_{rec} = #vec{p}_{1} + #vec{p}_{2}) [deg.]"
+ThetaPrctTit    = "#theta(#vec{q} - #vec{p}_{1} - #vec{p}_{2}) [deg.]"
+
+def pTit(i):
+    return "|#vec{p}(%d)| [GeV/c]"%i
+
+def ThetaTit(name):
+    return "#theta_{%s} [deg.]"%name
+
+def cosThetaTit(name):
+    return "cos ( #theta_{%s} )"%name
+
+
 def plot(ana , name, plot_args):
     
     c = ana.CreateCanvas(name)

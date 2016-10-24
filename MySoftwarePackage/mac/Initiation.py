be8b4fe3e117096be5b728acbe078fea4dd659de
Erez Cohen <erezcohen@Erezs-iMac.local>
2016-10-24 13:16:03 +0300

    Mon Oct 24 13:16:03 IDT 2016

--- MySoftwarePackage/mac/Initiation.py
+++ MySoftwarePackage/mac/Initiation.py
@@ -1,14 +1,7 @@
 import ROOT , sys , os , time
 ROOT.gStyle.SetOptStat(0000)
 
-def dirname( ):
-    dirfmt = "/Users/erezcohen/Desktop/%4d-%02d-%02d"
-    dirname = dirfmt % time.localtime()[0:3]
-    return dirname;
-
-def createnewdir( ):
-    dirfmt = "/Users/erezcohen/Desktop/%4d-%02d-%02d"
-    dirname = dirfmt % time.localtime()[0:3]
+def generate_directory(dirname):
     try:
         os.makedirs(dirname)
     except OSError, e:
@@ -16,12 +9,22 @@ def createnewdir( ):
             raise # This was not a "directory exist" error..
         return
 
+
+def createnewdir():
+    dirfmt = "/Users/erezcohen/Desktop/%4d-%02d-%02d"
+    dirname = dirfmt % time.localtime()[0:3]
+    return dirname
+
+
+def create_dated_dir( name ):
+    time = "%4d-%02d-%02d"
+    time_name = time % time.localtime()[0:3]
+    dirfmt = "/Users/erezcohen/Desktop/%s-%s"%(name,time_name)
+    generate_directory(dirname)
+    return dirname
+
+
 def create_dir( path , dir_name ):
-    dirname = path + "/" + dir_name
-    try:
-        os.makedirs( dirname )
-    except OSError, e:
-        if e.errno != 17:
-            raise # This was not a "directory exist" error..
-        return
+    generate_directory(dirname)
+    return dirname
 
